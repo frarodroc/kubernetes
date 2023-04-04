10b065b1f980c22b36d73e2a86b917c2286ee793
frarodroc <107277617+frarodroc@users.noreply.github.com>
2023-04-04 11:03:04 +0200

    Create index.php

---
+++ code/php/size/index.php
@@ -0,0 +1,14 @@
+
+<html>
+  <head>
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+      set_time_limit(0);
+      sleep(5);
+      $hostname = getenv('HOSTNAME');
+      echo $hostname;
+    ?>
+  </body>
+</html>
